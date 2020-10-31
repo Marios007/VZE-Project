748a4c4d9bee53d4b7ff9f62fb6c35fa2dd228fe
Marios007 <mario.schuelke@outlook.com>
2020-10-31 20:06:21 +0100

    .

---
+++ gui/styles.py
@@ -0,0 +1,6 @@
+###################
+##### Styles ######
+###################
+
+styleHeadlines = "color: rgb(242, 141, 27)"
+styleBluebutton= "QPushButton{\n""    font: 75 26pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white\n""}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"

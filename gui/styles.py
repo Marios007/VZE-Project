9382702b0b4f852e92663d94bf1ade35dc38c1d6
Marios007 <mario.schuelke@outlook.com>
2020-11-15 10:23:59 +0100

    added styles for text and headline label

--- gui/styles.py
+++ gui/styles.py
@@ -2,6 +2,8 @@
 ##### Styles ######
 ###################
 
-styleHeadlines = "color: rgb(242, 141, 27);\nfont: 75 24pt ""MS Shell Dlg 2"";"
+styleHeadlines = "color: rgb(242, 141, 27);\nfont: bold 24pt \"MS Shell Dlg 2\";"
 styleBackground = "background-color: rgb(0, 0, 0)"
-styleBluebutton= "QPushButton{\n""    font: 75 26pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white\n""}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"
+styleText1="color: rgb(255, 255, 255);\nfont: bold 18pt \"MS Shell Dlg 2\";"
+styleBluebuttonbig= "QPushButton{\n""    font: 75 26pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white\n""}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"
+styleBluebuttonsmall="QPushButton{\n""    font: 75 18pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white\n""}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"

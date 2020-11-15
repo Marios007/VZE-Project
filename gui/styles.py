523eb33adbda50cf5afe94fed1b67af6f27e50fb
Marios007 <mario.schuelke@outlook.com>
2020-11-15 11:16:45 +0100

    added more styles and cleaned up the demoscreen

--- gui/styles.py
+++ gui/styles.py
@@ -2,8 +2,15 @@
 ##### Styles ######
 ###################
 
+#orange headlines
 styleHeadlines = "color: rgb(242, 141, 27);\nfont: bold 24pt \"MS Shell Dlg 2\";"
+
 styleBackground = "background-color: rgb(0, 0, 0)"
-styleText1="color: rgb(255, 255, 255);\nfont: bold 18pt \"MS Shell Dlg 2\";"
+# Text under headline or similar short text
+styleText1="color: rgb(255, 255, 255);\nfont: 75 18pt \"MS Shell Dlg 2\";"
+#big blue buttons
 styleBluebuttonbig= "QPushButton{\n""    font: 75 26pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white\n""}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"
+#small blue buttons
 styleBluebuttonsmall="QPushButton{\n""    font: 75 18pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white\n""}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"
+#other small buttons only need hover function
+styleSmallButton="QPushButton:hover{\n""    border-radius:5px;\n""    border: 2px solid rgb(255, 255, 255)\n""\n""}"

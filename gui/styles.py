387b87e5ba36a2a573e1b65c878b922bd9161f5f
Marios007 <mario.schuelke@outlook.com>
2020-11-18 21:22:50 +0100

    change icons for button

--- gui/styles.py
+++ gui/styles.py
@@ -4,12 +4,13 @@
 
 #orange headlines
 styleHeadlines = "color: rgb(242, 141, 27);\nfont: bold 24pt \"MS Shell Dlg 2\";"
-
+# Black background
 styleBackground = "background-color: rgb(0, 0, 0)"
 # Text under headline or similar short text
 styleText1="color: rgb(255, 255, 255);\nfont: 75 18pt \"MS Shell Dlg 2\";"
 #big blue buttons
-styleBluebuttonbig= "QPushButton{\n""    font: 75 26pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white\n""}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"
+styleBluebuttonbig= "QPushButton{\n""    font: 75 26pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white;\n""    text-align:bottom;\n""   padding: 20px;\n""  background-image: url(:/icons/demo_regen);\n""  background-position: center\n;""     background-repeat: no-repeat}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"
+
 #small blue buttons
 styleBluebuttonsmall="QPushButton{\n""    font: 75 18pt \"MS Shell Dlg 2\" ;\n""    background-color: #1C4481;\n""    border-radius: 5px;\n""    color:white\n""}\n""\n""QPushButton:hover{\n""    border: 2px solid rgb(255, 255, 255)\n""}"
 #other small buttons only need hover function
@@ -18,3 +19,5 @@ styleSmallButton="QPushButton:hover{\n""    border-radius:5px;\n""    border: 2p
 styleGridHeadline="color: rgb(255, 255, 255);\n""font: 12pt \"MS Shell Dlg 2\";"
 #style for SpinBox in DataInputScreen
 styleSpinBox="QSpinBox{\n""    font: 75 12pt \"MS Shell Dlg 2\" ;\n""    background-color: #000000;\n""    color:white\n""}"
+
+
